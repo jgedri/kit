befa605d882263465d4f48c5c89472c98e58238b
jgedri <jgedri@gmail.com>
2020-06-09 16:33:56 -0400

    Add coin sprite, grid.

--- main.lua
+++ main.lua
@@ -8,10 +8,13 @@ function love.load ()
   sprites.bar = love.graphics.newImage('sprites/bar.png')
   sprites.bar2 = love.graphics.newImage('sprites/bar2.png')
   sprites.kitfox = love.graphics.newImage('sprites/kit_from_firefox.png')
+  sprites.coin = love.graphics.newImage('sprites/coin_sheet.png')
 
   local foxGrid = anim8.newGrid(18, 80, sprites.kitfox:getWidth(), sprites.kitfox:getHeight())
   walk = anim8.newAnimation(foxGrid('1-3',1), 0.1)
   slide = anim8.newAnimation(foxGrid('1-3',8), 0.1)
+
+  local coinGrid = anim8.newGrid(41, 42, sprites.coin:getWidth(), sprites.coin:getHeight())
 end
 
 function love.update(dt)
@@ -21,4 +24,5 @@ end
 function love.draw()
   love.graphics.draw(sprites.bar, 0, 0)
   love.graphics.draw(sprites.kitfox, 0, 150)
+  love.graphics.draw(sprites.coin, 10, 100)
 end
