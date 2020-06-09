b2a36fdb8fea232642c7e1b9903d9d54d711dbf5
jgedri <jgedri@gmail.com>
2020-06-09 12:35:31 -0400

    Add sprites for the bar.

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ function love.load ()
   anim8 = require "anim8.anim8"
   windowWidth = love.graphics.getWidth()
   windowHeight = love.graphics.getHeight()
+
+  sprites = {}
+  sprites.bar = love.graphics.newImage('sprites/bar.png')
+  sprites.bar2 = love.graphics.newImage('sprites/bar2.png')
 end
 
 function love.update(dt)
@@ -10,4 +14,4 @@ end
 
 function love.draw()
 
-end  
+end
