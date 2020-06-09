98c6405f664e434170739573fc0f3ea861d5f691
jgedri <jgedri@gmail.com>
2020-06-09 12:22:31 -0400

    Set up the project.

---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load ()
+  anim8 = require "anim8.anim8"
+  windowWidth = love.graphics.getWidth()
+  windowHeight = love.graphics.getHeight()
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end  
